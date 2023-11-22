13c3b238810454c6b2bcea7de565175ee2a414d8
KevinZucca <kev.94@live.it>
2023-11-22 14:53:27 +0100

    feat: create list and find post by slug functions

--- index.js
+++ index.js
@@ -2,30 +2,30 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const posts = require("./posts.json");
 
-// function create() {
-//   prisma.post
-//     .createMany({
-//       data: posts.map((post) => {
-//         return post;
-//       }),
-//     })
-//     .then((result) => {
-//       console.log("creata nuova lista", result);
-//     })
-//     .catch((err) => console.error(err));
-// }
+function create() {
+  prisma.post
+    .createMany({
+      data: posts.map((post) => {
+        return post;
+      }),
+    })
+    .then((result) => {
+      console.log("creata nuova lista", result);
+    })
+    .catch((err) => console.error(err));
+}
 
-// function read(slug) {
-//   prisma.post
-//     .findUnique({
-//       where: {
-//         slug: slug,
-//       },
-//     })
-//     .then((post) => {
-//       console.log("il post è: ", post);
-//     });
-// }
+function read(slug) {
+  prisma.post
+    .findUnique({
+      where: {
+        slug: slug,
+      },
+    })
+    .then((post) => {
+      console.log("il post è: ", post);
+    });
+}
 
-// create();
-// read("intervista-sviluppatore-indie-game");
+create();
+read("intervista-sviluppatore-indie-game");
