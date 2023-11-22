e1631198dac0096b54b1a3e35c47375f30e464a1
KevinZucca <kev.94@live.it>
2023-11-22 14:52:46 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+const posts = require("./posts.json");
+
+// function create() {
+//   prisma.post
+//     .createMany({
+//       data: posts.map((post) => {
+//         return post;
+//       }),
+//     })
+//     .then((result) => {
+//       console.log("creata nuova lista", result);
+//     })
+//     .catch((err) => console.error(err));
+// }
+
+// function read(slug) {
+//   prisma.post
+//     .findUnique({
+//       where: {
+//         slug: slug,
+//       },
+//     })
+//     .then((post) => {
+//       console.log("il post è: ", post);
+//     });
+// }
+
+// create();
+// read("intervista-sviluppatore-indie-game");
